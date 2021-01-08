39b70db1094863210c50cee71ded1d58f0bfe86b
Eduard <eduard.ghiurca@de.rhenus.com>
2021-01-08 16:42:57 +0100

    first commit

---
+++ MoviesApi/Controllers/GenresController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApi.Entities;
+using MoviesApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/genres")]
+    public class GenresController: ControllerBase
+    {
+        private readonly IRepository repository;
+
+        public GenresController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public List<Genre> Get()
+        {
+            return repository.GetAllGenres();
+        }
+
+        [HttpGet]
+        public Genre Get(int id)
+        {
+            var genre = repository.GetGenreById(id);
+
+            if(genre == null)
+            {
+                //return NotFound();
+            }
+            return genre;
+
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+            
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+
+
+    }
+}
---
+++ MoviesApi/Services/InMemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using MoviesApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Services
+{
+    public class InMemoryRepository: IRepository
+    {
+        private List<Genre> _genres;
+        public InMemoryRepository()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre() {Id = 1, Name = "Comedy"},
+                new Genre() {Id = 2, Name = "Adventure"}
+            };
+
+        }
+
+        public List<Genre> GetAllGenres()
+        {
+            return _genres;
+        }
+
+        public Genre GetGenreById(int id)
+        {
+            return _genres.FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
